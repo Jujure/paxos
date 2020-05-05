0354ce0bea3c1b510f8b10cb37523826bc7e5355
Julien Clement <julien.clement@epita.fr>
2020-05-05 20:50:55 +0200

    Message sending

--- src/events/send.cc
+++ src/events/send.cc
@@ -21,7 +21,33 @@ namespace paxos
 
     void SendEW::operator()()
     {
+        shared_socket sock = connection_->get_socket();
 
+        auto buffer = connection_->get_buffer_out();
+        std::string response = buffer.to_string();
+
+        log("Sending a message: ", blue);
+        log(response, white);
+        ssize_t write = 0;
+        try
+        {
+            write = sock->send(response.c_str(), response.size());
+        }
+        catch (const std::system_error& e)
+        {
+            std::cerr << "Connection closed while sending\n";
+            event_register.unregister_ew(this);
+            return;
+        }
+
+        if (write == -1)
+            return;
+
+        buffer.pop_front(write);
+        if (!buffer.empty())
+            return;
+
+        event_register.unregister_ew(this);
     }
 
     static shared_socket connect_to_ip_port(std::string ip, std::string port)
@@ -63,6 +89,7 @@ namespace paxos
         catch (const ConnectionFailed& e)
         {
             log("Could not connect to " + legislator->config_.name, red);
+            return;
         }
 
         shared_connection connection = std::make_shared<Connection>(socket);
